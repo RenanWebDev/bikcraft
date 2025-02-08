549b70265a4319e18fd6980ae0d1c09da6a59e3d
unknown <renannsouto02@gmail.com>
2025-02-08 16:41:46 -0300

    Perguntas frequentes js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 // Ativar Links do Menu
 const links = document.querySelectorAll(".header-menu a");
 
-function ativarLink(link){
+function ativarLink(link) {
   const url = location.href;
   const href = link.href;
 
-  if (url.includes(href)){
+  if (url.includes(href)) {
     link.classList.add("ativo");
   }
 }
@@ -16,11 +16,31 @@ links.forEach(ativarLink);
 
 const parametros = new URLSearchParams(location.search);
 
-function ativarProduto(parametro){
+function ativarProduto(parametro) {
   const elemento = document.getElementById(parametro);
   if (elemento) {
-    elemento.checked = true
+    elemento.checked = true;
   }
 }
 
 parametros.forEach(ativarProduto);
+
+//perguntas frequentes
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
